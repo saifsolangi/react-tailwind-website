bca8656a08cf6c510d1cbcf310d30a627bb7429a
Saif Ali <saifsolangi2@gmail.com>
2023-08-12 06:03:05 +0500

    packages section added along with animations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Banner from "./components/Banner";
 import Expert from "./components/Expert";
 import Header from "./components/Header";
 import NewsLetter from "./components/NewsLetter";
+import Plans from "./components/Plans";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Banner></Banner>
       <Expert></Expert>
       <NewsLetter></NewsLetter>
+      <Plans></Plans>
     </>
   );
 }
